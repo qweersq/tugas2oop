7a1cc7af25c07f9f671fda3acd1608b93b6f66f9
Hadad Al Akbar <akbarhadadal@gmail.com>
2022-10-11 17:36:47 +0700

    initial project

---
+++ src/main/java/com/mycompany/tugas2oop/Tugas2oop.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.tugas2oop;
+
+/**
+ *
+ * @author hadadalakbar
+ */
+public class Tugas2oop {
+
+    public static void main(String[] args) {
+        System.out.println("TUGAS 2 OOP ATHAILAH, HADAD, FALDI!");
+    }
+}
